3861de4dab42a5e808a27c460f5001db6c029610
kic0412 <kic0412@naver.com>
2023-06-22 17:54:53 +0900

    06-22

---
+++ chapter4/app/src/main/java/kr/co/kkm/chapter4/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.kkm.chapter4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
