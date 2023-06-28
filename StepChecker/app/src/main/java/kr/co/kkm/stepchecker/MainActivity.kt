eeb158c1cb9efe6a04c801e0bc7165c940033dee
kic0412 <kic0412@naver.com>
2023-06-28 17:58:11 +0900

    06-28

--- StepChecker/app/src/main/java/kr/co/kkm/stepchecker/MainActivity.kt
+++ StepChecker/app/src/main/java/kr/co/kkm/stepchecker/MainActivity.kt
@@ -2,12 +2,16 @@ package kr.co.kkm.stepchecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+        myRef.setValue("Developers must get jobs")
     }
 }
